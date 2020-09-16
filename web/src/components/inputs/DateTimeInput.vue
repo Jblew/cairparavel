bb09defca9de7f55d09f5e2d949ecf09b1d9315b
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-09-16 22:44:23 +0200

    feat(web): datetime input

---
+++ web/src/components/inputs/DateTimeInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <span>
+    <input type="date" v-model="dateIso" />
+    <input type="time" v-model="timeIso" />
+  </span>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+import { dateToISODate, dateToISOTime } from '@/util'
+
+@Component({
+  components: {},
+})
+export default class DateTimeInput extends Vue {
+  @Prop()
+  value: number
+
+  get dateIso(): string {
+    return dateToISODate(new Date(this.value))
+  }
+
+  set dateIso(v: string) {
+    const newDate = new Date(`${v}T${this.timeIso}`)
+    this.$emit('input', newDate.getTime())
+  }
+
+  get timeIso(): string {
+    return dateToISOTime(new Date(this.value))
+  }
+
+  set timeIso(v: string) {
+    const newDate = new Date(`${this.dateIso}T${v}`)
+    this.$emit('input', newDate.getTime())
+  }
+}
+</script>
