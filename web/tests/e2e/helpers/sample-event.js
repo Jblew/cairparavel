5f07a34912bf6258ada74e3e96367c6364bfc04f
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-09-19 13:29:22 +0200

    test(web): test list events

---
+++ web/tests/e2e/helpers/sample-event.js
@@ -0,0 +1,35 @@
+const { DateTime } = require('luxon');
+const { v4: uuid } = require('uuid');
+
+function getSampleEvent(modifiers) {
+  const startTime = DateTime.local().plus({ days: 20 })
+  const endTime = startTime.plus({ hours: 1 })
+  const signupTime = startTime.minus({ days: 1 })
+  const ownerUid = process.env.CYPRESS_TEST_UID
+  return {
+    startTime: startTime.toMillis(),
+    endTime: endTime.toMillis(),
+    signupTime: signupTime.toMillis(),
+    votingTime: - 1,
+    allowedTimes: [],
+    canSuggestTime: true,
+    name: 'Test event with a name',
+    description: 'Test event with a description',
+    minParticipants: 1,
+    maxParticipants: 5,
+    ownerDisplayName: 'Test event owner',
+    ownerUid,
+    timeConfirmed: true,
+    ...modifiers
+  }
+}
+
+function addSampleEvent(modifiers) {
+  const id = uuid();
+  const data = getSampleEvent(modifiers)
+  cy.callFirestore('set', 'envs/test/events/' + id, data)
+  return id
+}
+
+
+module.exports = { getSampleEvent, addSampleEvent }
--- web/tests/e2e/specs/home.js
+++ web/tests/e2e/specs/home.js
@@ -1,3 +1,5 @@
+import { addSampleEvent } from '../helpers/sample-event.js'
+
 describe('Home page', () => {
   beforeEach(() => cy.login() && cy.setCookie('e2e', '1'))
 
@@ -11,13 +13,24 @@ describe('Home page', () => {
     cy.contains('.router-link-exact-active', 'Home')
   })
 
-  it('Shows events', () => {
-    cy.visit('/')
-    cy.contains('.router-link-exact-active', 'Home')
-  })
-
   it('Shows create event button', () => {
     cy.visit('/')
     cy.contains('[data-test="create-event-btn"]', 'Create event')
+  })
+
+  it('Shows events', () => {
+    const name = 'Event #' + Date.now()
+    addSampleEvent({ name })
+    cy.visit('/')
+    cy.get('[data-test="events-list"]').should('be.visible')
+    cy.get('[data-test="events-list"]').find('.event').its('length').should('be.gte', 1)
+  })
+
+  it('Newest event is at the top of the list', () => {
+    const name = 'Event #' + Date.now()
+    addSampleEvent({ name })
+    cy.visit('/')
+    cy.get('[data-test="events-list"]').should('be.visible')
+    cy.get('[data-test="events-list"] .event').first().get('.name').contains(name)
   })
 })
