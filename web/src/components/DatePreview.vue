4ea642c6e0bdab47d4d21f392979b94bedbaf813
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-10-05 22:48:03 +0200

    feat(web): add date preview component

---
+++ web/src/components/DatePreview.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="date-preview">
+    <span>{{ start || formatDate }}</span>
+
+    <span v-if="stop">{{ intervalMs || formatInvervalMs }}</span>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue, Inject } from 'vue-property-decorator'
+import LoadingSpinner from './LoadingSpinner.vue'
+
+@Component({
+  filters: {
+    formatDate,
+    formatInvervalMs,
+  }
+})
+export default class extends Vue {
+  @Prop({ required: true })
+  start: Date
+
+  @Prop({ required: false })
+  stop: Date
+
+  get intervalMs(): number {
+    return (this.stop?.getTime() || 0) - this.start.getTime()
+  }
+}
+
+function formatDate(date: Date): string {
+  return date.toISOString()
+}
+
+function formatInvervalMs(intervalMs: number): string {
+  const hours = intervalMs / 1000 / 3600
+  return hours.toFixed(1) + 'h'
+}
+</script>
+<style>
+</style>
