1f59a54f178ea2afa817093b1fe496c3eb04dd29
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-10-08 19:05:02 +0200

    feat(functions): define Event in domain

---
+++ functions/app/domain/Event.go
@@ -0,0 +1,32 @@
+package domain
+
+type Event struct {
+	ID string `json:"id"`,
+  OwnerUid string `json:"ownerUid"`,
+  OwnerDisplayName string `json:"ownerDisplayName"`,
+  VotingTime int64 `json:"votingTime"`,
+  StartTime int64 `json:"startTime"`,
+  EndTime int64 `json:"endTime"`,
+  TimeConfirmed bool `json:"timeConfirmed"`,
+  SignupTime int64 `json:"signupTime"`,
+  Votes?: map[string]EventTimeVotes `json:"votes"`,
+  SignedMembers?:  map[string]EventSignup `json:"signedMembers"`,
+  MinParticipants int `json:"minParticipants"`,
+  MaxParticipants int `json:"maxParticipants"`,
+  Name string `json:"name"`,
+  Description string `json:"description"`,
+  AllowedTimes int64[] `json:"allowedTimes"`,
+  CanSuggestTime bool `json:"canSuggestTime"`,
+}
+
+
+type EventTimeVotes struct {
+  UID string `json:"uid"`,
+  DisplayName string `json:"displayName"`,
+  Times int64[] `json:"times"`,
+}
+
+type EventSignup struct {
+  UID string `json:"uid"`,
+  DisplayName string `json:"displayName"`,
+}
