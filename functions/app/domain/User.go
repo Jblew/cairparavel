56508b6f3e33704b877d17e1d7774c7880493baa
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-10-10 21:57:14 +0200

    refactor(functions): handle user events in DDD approach

--- functions/app/domain/User.go
+++ functions/app/domain/User.go
@@ -1,13 +1,27 @@
 package domain
 
-import "time"
+import (
+	"time"
 
+	"github.com/golobby/container/pkg/container"
+)
+
+// User stored in db. Do not confuse with firestore user
 type User struct {
 	Email    string    `json:"Email"`
 	UID      string    `json:"UID"`
 	JoinedAt time.Time `json:"JoinedAt"`
 }
 
+// OnFirstLogin handles situation when user logs in and doesnt have an account stored
+func (user *User) OnFirstLogin(temporaryUser User, container container.Container) error {
+	var usersRepository UsersRepository
+	container.Make(&usersRepository)
+
+	return usersRepository.StoreUser(*user)
+}
+
+// UsersRepository repository for users
 type UsersRepository interface {
 	StoreUser(user User) error
 	GetUser(userID string) (User, error)
