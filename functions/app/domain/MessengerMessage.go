eb1777c919763f615d43b7b703ce9beff65a11d7
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-10-10 21:56:54 +0200

    refactor(functions): handle messenger events in domain in DDD approach

--- functions/app/domain/MessengerMessage.go
+++ functions/app/domain/MessengerMessage.go
@@ -1,1 +1,20 @@
 package domain
+
+import "github.com/golobby/container/pkg/container"
+
+// MessengerMessage is message send from messenger to our bot
+type MessengerMessage struct {
+	Text      string `"json":"text"`
+	Recipient MessengerRecipient
+}
+
+// OnNew handles new message
+func (message *MessengerMessage) OnNew(container container.Container) error {
+	notification := Notification{
+		Template: "messenger_respond",
+		Payload:  make(map[string]interface{}),
+	}
+	notification.Payload["messageText"] = message.Text
+
+	return message.Recipient.Notify(notification, container)
+}
--- functions/app/domain/Notifications.go
+++ functions/app/domain/Notifications.go
@@ -1,5 +1,9 @@
 package domain
 
+import (
+	"github.com/golobby/container/pkg/container"
+)
+
 // Notification is a notification to be sent to an user
 type Notification struct {
 	ID       string                 `json:"id"`
@@ -8,7 +12,35 @@ type Notification struct {
 	Payload  map[string]interface{} `json:"payload"`
 }
 
+// PlainNotification is notification resolved with template — used for history
+type PlainNotification struct {
+	Contents string
+	UID      string
+}
+
+// OnQueued handles new notification in queue
+func (notification *Notification) OnQueued(container container.Container) error {
+	var queue NotificationQueue
+	container.Make(&queue)
+
+	var messengerRecipientRepository MessengerRecipientRepository
+	container.Make(&messengerRecipientRepository)
+
+	recipient, err := messengerRecipientRepository.GetForUser(notification.UID)
+	if err != nil {
+		return err
+	}
+
+	err = recipient.Notify(*notification, container)
+	if err != nil {
+		return err
+	}
+
+	return queue.Delete(notification.UID, notification.ID)
+}
+
 // NotificationQueue queues notifications
 type NotificationQueue interface {
-	ScheduleToSend(uid string, notification Notification) error
+	Add(uid string, notification Notification) error
+	Delete(uid string, id string) error
 }
